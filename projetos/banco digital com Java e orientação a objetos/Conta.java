c853716a2109162cb35a5939510f0d13cd9aba63
Seph-KS <andreysarda@gmail.com>
2022-07-20 15:25:08 -0300

    adiciona projeto banco digital Java

---
+++ projetos/banco digital com Java e orientação a objetos/Conta.java
@@ -0,0 +1,55 @@
+public abstract class Conta implements interfaceConta {
+
+    private static final int AGENCIA_PADRAO = 1;
+    private static int SEQUENCIAL = 1;
+
+    protected int agencia;
+    protected int conta;
+    protected double saldo;
+    protected Cliente cliente;
+
+    public Conta(Cliente cliente) {
+        agencia = AGENCIA_PADRAO;
+        conta = SEQUENCIAL++;
+        this.cliente = cliente;
+    }
+
+    public int getAgencia() {
+        return this.agencia;
+    }
+
+    public int getConta() {
+        return this.conta;
+    }
+
+    public double getSaldo() {
+        return this.saldo;
+    }
+
+    protected void imprimirInfo() {
+        System.out.println(String.format("Titular: %s ", this.cliente.getNome()));
+        System.out.println(String.format("Agencia: %d", this.agencia));
+        System.out.println(String.format("Conta: %d", this.conta));
+        System.out.println(String.format("Saldo: %.2f", this.saldo));
+    }
+
+    @Override
+    public void sacar(double valor) {
+        saldo -= valor;
+        
+    }
+
+    @Override
+    public void depositar(double valor) {
+        saldo += valor;
+        
+    }
+
+    @Override
+    public void transferir(double valor, Conta contaDestino) {
+        this.sacar(valor);
+        contaDestino.depositar(valor);    
+    }
+
+}
+
---
+++ projetos/banco digital com Java e orientação a objetos/contaCorrente.java
@@ -0,0 +1,13 @@
+public class contaCorrente extends Conta {
+
+    public contaCorrente(Cliente cliente) {
+        super(cliente);
+    }
+
+    @Override
+    public void imprimirExtrato() {
+        System.out.println(" === Extrato Conta Corrente === ");
+        super.imprimirInfo();
+    }
+
+}
